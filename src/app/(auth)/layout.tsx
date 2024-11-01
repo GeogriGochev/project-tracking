b2d81175af57aa58ca367f84c4440bab5352d9c1
Georgi Gochev <georgi.gochev@egt.com>
2024-11-01 11:29:02 +0200

    logout button sart

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -17,7 +17,7 @@ const layout = ({children}: AuthLayoutProps) => {
       
       <div className='mx-auto max-w-screen-2xl p-4'>
         <nav className='flex justify-between items-center'>
-          <Image src='/logo.svg' height={56} width={152}  alt='logo'/>
+          <Link href='/'> <Image src='/logo.svg' height={56} width={152}  alt='logo'/></Link>
           <Button asChild variant='secondary'><Link href={isSignIn ? '/sign-up' : '/sign-in' }>{isSignIn ? 'Sign Up' : 'Login' }</Link></Button>
         </nav>
         <div className='flex flex-col items-center justify-center pt-4 md:pt-14'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,29 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { useCurrent } from '@/features/auth/api/use-current';
+import { useEffect } from 'react';
+import { useLogout } from '@/features/auth/api/use-logout';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { UserButton } from '@/features/auth/components/UserButton';
 
 
 export default function Home() {
+  const router = useRouter()
+  const {data,isLoading} = useCurrent()
+  const {mutate} = useLogout()
+  
+
+
+  useEffect(() => {
+    if(!data && !isLoading) {
+      router.push('/sign-in')
+    }
+  }, [data,router,isLoading])
+  
+
   return (
     <div className='flex gap-4 p-4'>
-      <Button>Primary</Button>
-      <Button variant='secondary' >Secondary</Button>
-      <Button variant='destructive'>Destructive</Button>
-      <Button variant='ghost'>Ghost</Button>
-      <Button variant='muted'>Muted</Button>
-      <Button variant='outline'>Outline</Button>
-      <Button variant='teritary'>Teritary</Button>
-      <div>
-        <Input/>
-      </div>
+      <UserButton/>
     </div>
   );
 }
